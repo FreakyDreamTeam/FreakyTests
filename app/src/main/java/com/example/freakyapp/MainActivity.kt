8e3d9643c1a17db3817ff8ca645d8c469890c420
pietro889 <pietro.favarel@gmail.com>
2024-08-09 12:34:40 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/freakyapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.freakyapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.freakyapp.ui.theme.FreakyAppTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FreakyAppTheme {
+                MyBottomAppBar()
+            }
+        }
+    }
+}
